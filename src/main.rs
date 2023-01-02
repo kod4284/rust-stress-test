f2f3ad40c5592c54ebad8e9c2aac85f622867d4c
Daewoong Ko <kodw4284@gmail.com>
2023-01-03 00:47:57 +0900

    feat: make health end point for stress test

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::{thread, time};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    thread::sleep(time::Duration::from_millis(500));
+    HttpResponse::Ok().body("OK!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
